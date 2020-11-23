9d7afbf65fbb2d7955b2de7431c982a10fdcbf68
Angga <theshadow1325@gmail.com>
2020-11-23 11:05:34 +0800

    Prepare update / delete backend

---
+++ app/src/main/java/com/example/forcoms/EditDeleteTopicFragment.java
@@ -0,0 +1,20 @@
+package com.example.forcoms;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EditDeleteTopicFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edit_delete_topic, container, false);
+
+    }
+}
